e0646b3282db373160e0f862268efa2d6028168b
Jonas <jonas.luft@hcu-hamburg.de>
2021-05-07 16:34:33 +0200

    no static paths in profiling

--- eval_scripts/parameter_tuning.py
+++ eval_scripts/parameter_tuning.py
@@ -4,6 +4,7 @@ import ast
 import argparse
 
 import config
+from eval_scripts.eval_helpers import results
 
 def init():
     os.makedirs(config.path_logs, exist_ok=True)
@@ -16,28 +17,6 @@ def init():
     logging.basicConfig(filename=(config.path_logs + '/prf.log') , 
                         level=logging.INFO, 
                         format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-7.7s]  %(message)s") # gimme all your loggin'!
-
-def results(logpath, results_file):
-    from eval_logs import eval_logs
-    eval_logs(logpath, results_file)
-    prediction_results = {}
-    with open(results_file) as fr:
-        fr.readline() # skip header
-        for line in fr:
-            line = line.strip()
-            vals = line.split("; ")
-            gt = vals[0]
-            pred = vals[1]
-            scores = ast.literal_eval(vals[6])
-            max_score = max(scores) if gt == pred else -1
-            rank = int(vals[-1])
-            prediction_results[gt] = (rank, max_score)
-
-    scores = [x[1] for x in prediction_results.values()]
-    num_incorrect = scores.count(-1)
-    # print("prediction scores (index rank, ransac score)",prediction_results)
-    avg_score = sum(scores)/len(prediction_results)
-    return avg_score, num_incorrect
 
 def save_results(results, path):
     import csv
--- profiling.py
+++ profiling.py
@@ -8,8 +8,10 @@ import logging
 import ast
 import re
 import importlib
+import argparse
 
 import config
+from eval_scripts.eval_helpers import results
 
 def init():
     os.makedirs(config.path_logs, exist_ok=True)
@@ -42,30 +44,6 @@ def print_profile(prf_file="profile.prf", outfile=None, function_of_interest="")
     if outfile:
         fw.close()
 
-def results(logpath, results_file):
-    from eval_logs import eval_logs
-    # os.system("py -3.7 eval_logs.py > NUL")
-    eval_logs(logpath)
-    os.system("mv eval_result.csv %s" % results_file)
-    prediction_results = {}
-    with open(results_file) as fr:
-        fr.readline() # skip header
-        for line in fr:
-            line = line.strip()
-            vals = line.split("; ")
-            gt = vals[0]
-            pred = vals[1]
-            scores = ast.literal_eval(vals[6])
-            max_score = max(scores) if gt == pred else -1
-            rank = int(vals[-1])
-            prediction_results[gt] = (rank, max_score)
-
-    scores = [x[1] for x in prediction_results.values()]
-    num_incorrect = scores.count(-1)
-    # print("prediction scores (index rank, ransac score)",prediction_results)
-    avg_score = sum(scores)/len(prediction_results)
-    return avg_score, num_incorrect
-
 def read_time_calls(prf_file):
     with open(prf_file) as fr:
         lines= "".join(fr.readlines())
@@ -89,16 +67,13 @@ def load_errors_csv(filepath):
     return results
 
 if __name__ == "__main__":
-
-    ### VARS
-    # sheet = "319"
-    # input_file = "E:/data/deutsches_reich/SLUB/cut/%s.png" % sheet
-    # input_file = "E:/data/deutsches_reich/SLUB/cut/raw/%s.bmp" % sheet
-    # input_file = "E:/data/deutsches_reich/SBB/cut/list.txt"
-    # input_file = "E:/data/deutsches_reich/SBB/cut/list_small.txt"
-    input_file = "E:/data/deutsches_reich/wiki/highres/list_small.txt"
-    sheets_file = "data/blattschnitt_dr100_regular.geojson"
-    restrict=0
+    parser = argparse.ArgumentParser()
+    parser.add_argument("list", help="path to list of image files and truth sheet names")
+    parser.add_argument("sheets", help="sheets json file path string")
+    parser.add_argument("annotations", help="path to file containing ground truth corner annotations")
+    parser.add_argument("output", help="path to output profiling files")
+    parser.add_argument("restrict", help="n most similar images to use in spatial verification. Set to 0 to profile registration.", type=int)
+    args = parser.parse_args()
 
     # param_to_tune = "ransac_max_trials"
     # possible_values = [50,100,300,500,1000,1500,2000,3000]
@@ -107,19 +82,19 @@ if __name__ == "__main__":
     # possible_values = [1,2,3,4]
     
     param_to_tune = "registration_ecc_iterations"
-    possible_values = [50,100,500,1000,3000]
+    possible_values = [50,100]
 
     # param_to_tune = "jpg_compression"
     # possible_values = [90,None]
 
     results_compare = []
     # shutil.rmtree("profiling", ignore_errors=True)
-    outfolder = "eval/profiling/%s" % (param_to_tune)
+    outfolder = "%s/%s" % (args.output, param_to_tune)
 
     try:
         for val in possible_values:
-            config.path_logs = "%s/logs_perf_test_%s/" % (outfolder,val)
-            config.path_output = "E:/experiments/profiling_%s_%s/" % (param_to_tune,val)
+            config.path_logs = "%s/logs_perf_test_%s/" % (outfolder, val)
+            config.path_output = "%s/profiling_%s_%s/" % (outfolder, param_to_tune, val)
             # os.makedirs(config.path_output, exist_ok=True)
 
             init()
@@ -134,25 +109,25 @@ if __name__ == "__main__":
             ### RUN
             from main import process_sheet, process_list
             # from segmentation import load_and_run
-            cProfile.run('process_list(input_file,sheets_file,img=%s,restrict=restrict)'%(restrict==0),"%s/profile_%s.prf" % (outfolder, val))
-            # cProfile.run('process_sheet(input_file,sheets_file,img=False,number=sheet,restrict=restrict)',"profiling/profile_%s.prf" % val)
-            # cProfile.run('load_and_run(input_file,5)',"profile.prf")
+            cProfile.run('process_list(args.list,args.sheets,img=%s,restrict=args.restrict)'%(args.restrict==0),"%s/profile_%s.prf" % (outfolder, val))
+            # cProfile.run('process_sheet(args.list,args.sheetse,img=%s,restrict=args.restrict)'%(args.restrict==0)',"%s/profile_%s.prf" % (outfolder, val))
+            # cProfile.run('load_and_run(args.list,5)',"profile.prf")
 
             print_profile("%s/profile_%s.prf" % (outfolder,val),"%s/pr_out_%s.txt" % (outfolder, val), function_of_interest="georeference")
             total_time, func_ncalls, func_cumtime = read_time_calls("%s/pr_out_%s.txt" % (outfolder, val))
 
-            if restrict==0:
+            if args.restrict==0:
                 # profile registration
-                ground_truth_annotations_file = "E:/data/deutsches_reich/wiki/highres/annotations_wiki.csv"
-                inputpath = "E:/data/deutsches_reich/wiki/highres/"
-                resultsfile = "%s/eval_georef_results_%s.csv" % (outfolder,val)
+                # ground_truth_annotations_file = "E:/data/deutsches_reich/wiki/highres/annotations_wiki.csv"
+                inputpath = os.path.dirname(args.list)#"E:/data/deutsches_reich/wiki/highres/"
+                resultsfile = "%s/eval_georef_results_%s.csv" % (outfolder, val)
 
                 import eval_georef
                 importlib.reload(eval_georef) # make sure to reload config
 
-                sheet_corners = eval_georef.read_corner_CSV(ground_truth_annotations_file)
+                sheet_corners = eval_georef.read_corner_CSV(args.annotations)
                 img_list = list(sheet_corners.keys())
-                sheet_names, error_results, rmse_results = eval_georef.eval_list(img_list, sheet_corners, inputpath, sheets_file)
+                sheet_names, error_results, rmse_results = eval_georef.eval_list(img_list, sheet_corners, inputpath, args.sheets, config.path_output)
                 eval_georef.dump_csv(sheet_names, error_results, rmse_results, outpath=resultsfile)
 
                 errors = load_errors_csv(resultsfile)
@@ -160,7 +135,7 @@ if __name__ == "__main__":
                 resultdict = {"value": val, "totaltime": total_time, "ncalls": func_ncalls, "cumtime": func_cumtime, "mean_error": sum(errors.values())/len(errors)}
             else:
                 # profile retrieval
-                avg_score, num_incorrect = results("profiling/logs_perf_test_%s/" % val, "profiling/results_%s.csv" % val)
+                avg_score, num_incorrect = results(config.path_logs, "%s/results_%s.csv" % (outfolder, val))
 
                 resultdict = {"value": val, "totaltime": total_time, "ncalls": func_ncalls, "cumtime": func_cumtime, "score": avg_score, "#wrong": num_incorrect}
                 print(resultdict)
@@ -176,7 +151,7 @@ if __name__ == "__main__":
     plt.legend(loc="upper left")
     plt.xlabel(param_to_tune)
     plt.twinx()
-    if restrict==0:
+    if args.restrict==0:
         plt.plot(possible_values,[x["mean_error"] for x in results_compare], label="avg. error")
     else:    
         plt.plot(possible_values,[x["score"] for x in results_compare], label="avg. score")
